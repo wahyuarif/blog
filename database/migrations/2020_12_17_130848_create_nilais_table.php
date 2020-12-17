d43adabd2535a7bdaa7da9d222b84db31abaf6c1
wahyuarif <wahyu4rifkurniawan@gmai.com>
2020-12-17 20:20:34 +0700

    nilai

---
+++ database/migrations/2020_12_17_130848_create_nilais_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNilaisTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('nilais', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim');
+            $table->string('nama');
+            $table->string('prodi');
+            $table->string('judul');
+            $table->string('nm_dosen');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('nilais');
+    }
+}
